8a5945e69dae59008c645f7a382cfad00d0c0389
David Lindblad <david.lindblad@hotmail.com>
2014-10-08 23:00:03 +0200

    walker

--- SyntaxWalker/CyclomaticComplexityWalker.cs
+++ SyntaxWalker/CyclomaticComplexityWalker.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.CodeAnalysis.CSharp;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis;
 
 namespace SyntaxWalker
 {
