c5abd2468e62a2d1718f4f27043d5b7127f7b58c
David Lindblad <david.lindblad@hotmail.com>
2014-10-08 21:47:40 +0200

    Final version

---
+++ SyntaxTree/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Linq;
+
+namespace SyntaxTreeDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(@" 
+                public static float FindSquareRoot_BS(int number)
+                {
+                    var precision = 0.0001f;
+                    var min = 0;
+                    var max = number;
+                    var result = 0;
+                    while (max-min > precision)
+                    {
+                        result = (min + max) / 2;
+                        if ((result*result) >= number)
+                        {
+                           max = result;
+                        }
+                        else
+                        {
+                           min = result;
+                        }
+                   }
+                   return result;
+                }");
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+
+            var implicitVariables =
+                from variableDeclaration in root.DescendantNodes()
+                                                .OfType<VariableDeclarationSyntax>()
+                from implicitVariable in variableDeclaration.DescendantNodes()
+                                                            .OfType<IdentifierNameSyntax>()
+                where implicitVariable.Identifier.ValueText == "var"
+                select implicitVariable;
+
+            Console.WriteLine("Found {0} implicit variables", implicitVariables.Count());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SyntaxWalker/CyclomaticComplexityWalker.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis;
+
+namespace SyntaxWalker
+{
+    class CyclomaticComplexityWalker : CSharpSyntaxWalker
+    {
+        private int complexity = 0;
+
+        public override void VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            Console.WriteLine(node.Identifier.ValueText);
+            base.VisitClassDeclaration(node);
+        }
+
+        public override void VisitMethodDeclaration(MethodDeclarationSyntax node)
+        {
+            complexity = 1;
+
+            base.VisitMethodDeclaration(node);
+
+            Console.WriteLine("\t{0}\t\tCtyclomatic Complexity: {1}", node.Identifier.ValueText, complexity);
+        }
+
+        public override void VisitIfStatement(IfStatementSyntax node)
+        {
+            complexity++;
+            base.VisitIfStatement(node);
+        }
+
+        public override void VisitWhileStatement(WhileStatementSyntax node)
+        {
+            complexity++;
+            base.VisitWhileStatement(node);
+        }
+
+        public override void VisitForEachStatement(ForEachStatementSyntax node)
+        {
+            complexity++;
+            base.VisitForEachStatement(node);
+        }
+
+        public override void VisitCaseSwitchLabel(CaseSwitchLabelSyntax node)
+        {
+            complexity++;
+            base.VisitCaseSwitchLabel(node);
+        }
+
+        public override void VisitDefaultSwitchLabel(DefaultSwitchLabelSyntax node)
+        {
+            complexity++;
+            base.VisitDefaultSwitchLabel(node);
+        }
+
+        public override void VisitContinueStatement(ContinueStatementSyntax node)
+        {
+            complexity++;
+            base.VisitContinueStatement(node);
+        }
+
+        public override void VisitGotoStatement(GotoStatementSyntax node)
+        {
+            complexity++;
+            base.VisitGotoStatement(node);
+        }
+
+        public override void VisitCatchClause(CatchClauseSyntax node)
+        {
+            complexity++;
+            base.VisitCatchClause(node);
+        }
+    }
+}
