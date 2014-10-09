ae2f66edf2149efab6b2d95f4ffd83b2421dc468
David Lindblad <david.lindblad@hotmail.com>
2014-10-09 10:18:59 +0200

    .

--- SemanticAnalysisDemo/Program.cs
+++ SemanticAnalysisDemo/Program.cs
@@ -34,6 +34,15 @@ namespace SemanticAnalysisDemo
 
             var root = (CompilationUnitSyntax)tree.GetRoot();
 
+            var implicitVariables =
+                from variableDeclaration in root.DescendantNodes()
+                                                .OfType<LocalDeclarationStatementSyntax>()
+                from implicitVariable in variableDeclaration.DescendantNodes()
+                                                            .OfType<IdentifierNameSyntax>()
+                where implicitVariable.Identifier.ValueText == "var"
+                select implicitVariable;
+
+            Console.WriteLine("Found {0} implicit variables", implicitVariables.Count());
             Console.ReadLine();
         }
     }
